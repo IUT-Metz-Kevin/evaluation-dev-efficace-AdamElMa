cbb57dea1d94af68ee825bfe75a0b77e762071f3
Adam GEOFFROY <geoffroyadam70@gmail.com>
2025-06-03 14:58:12 +0200

    test 1 : première case vide droite FALSE

--- minesweeper.test.ts
+++ minesweeper.test.ts
@@ -1,11 +1,18 @@
 import { assertEquals } from "jsr:@std/assert";
 
-const mine = ".*...."
+const mine = "......"
 function grille(numero: number){
 
     //let grille : Array<string| number> = [];
-    let compter = -1;
-
+    let compterD = -1;
+    let compterG = -1;
+    let compterH = -1;
+    let compterB = -1;
+    let compterHD = -1;
+    let compterHG = -1;
+    let compterBD = -1;
+    let compterBG = -1;
+    let compter = compterB + compterH + compterD + compterG + compterBD + compterBG + compterHD + compterHG;
     //Detection de mine 0;
 
         /*if(mine[numero] === "*"){
@@ -17,15 +24,15 @@ function grille(numero: number){
         if(mine[numero] === "."){
 
              
-
+            /*
             if (mine[numero + 1] === "."){
-                compter += 1;
+                compterD += 1;
             }
 
             if (mine[numero + 1] === "*"){
                 compter += 2;
             }
-
+            */
 
         }
         
@@ -37,7 +44,12 @@ Deno.test("test case vide droite", () => {
   assertEquals(grille(0), 0);
 });
 
-Deno.test("test case bombe droite", () => {
+/*Deno.test("test case bombe droite", () => {
   
   assertEquals(grille(0), 1);
-});
+});*/
+
+/*Deno.test("test case vide droite et gauche", () => {
+  
+  assertEquals(grille(0), 0);
+});*/
